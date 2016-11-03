a60b1a469ed08b856ee69a5dd107746eb823612c
snailium <snailium@hotmail.com>
2016-11-03 17:18:24 +0000

    Tweaked the calculation.

--- display_brightness.sh
+++ display_brightness.sh
@@ -14,9 +14,9 @@ do
   read LIGHT < $SERDEV
 
   if [ ! -z $LIGHT ]; then
-    BRIGHT=$((LIGHT*225/600+30));
-    if [[ $BRIGHT -le 0 ]]; then
-      BRIGHT=0
+    BRIGHT=$((LIGHT*240/800+15));
+    if [[ $BRIGHT -le 15 ]]; then
+      BRIGHT=15
     elif [[ $BRIGHT -ge 255 ]]; then
       BRIGHT=255
     fi
