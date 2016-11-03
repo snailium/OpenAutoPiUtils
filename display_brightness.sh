dbd0244bf21923a38c989dd94f8f1771b379862e
snailium <snailium@hotmail.com>
2016-11-03 18:08:59 +0000

    Added script to auto adjust display brightness based on Arduino light sensor readings.

---
+++ display_brightness.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SERDEV=/dev/ttyACM0
+
+
+while [ : ]
+do
+  # Wait for device
+  while [ ! -e $SERDEV ]
+  do
+    sleep 1
+  done
+
+  read LIGHT < $SERDEV
+
+  if [ ! -z $LIGHT ]; then
+    BRIGHT=$((LIGHT*225/600+30));
+    if [[ $BRIGHT -le 0 ]]; then
+      BRIGHT=0
+    elif [[ $BRIGHT -ge 255 ]]; then
+      BRIGHT=255
+    fi
+    echo "Arduino reading is $LIGHT, set display brightness to $BRIGHT"
+    echo $BRIGHT > /sys/class/backlight/rpi_backlight/brightness
+  fi
+done
