d22fe12c2163809d4ffaf76ce6f93aab60f4c8c3
Guannan Wang <snailium@hotmail.com>
2018-04-15 03:52:07 -0400

    Combined Visual Studio generated header and comment.

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,12 +1,20 @@
+/*
+Name:		Arduino.ino
+Created:	2018/4/15 3:35:08
+Author:		snailium
+*/
+
 // Initialize the variables
 int lightPin = 0; // Use A0
 int lightLevel;
 
+// the setup function runs once when you press reset or power the board
 void setup() {
   // One-time setup. 9600 is a value that is called the "baud rate"
   Serial.begin(9600);
 }
 
+// the loop function runs over and over again until power down or reset
 void loop() {
   // Collect a data point
   lightLevel=analogRead(lightPin);
