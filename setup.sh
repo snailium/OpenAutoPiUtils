cd28d5ad341ed6bac8f8eae3f794447c00f931a2
snailium <snailium@hotmail.com>
2018-04-19 03:48:47 -0400

    Updated not install NTP for Cranshaft 2018-04-07. Added USBmount and GPAC for dashcam script.

--- setup.sh
+++ setup.sh
@@ -15,22 +15,40 @@ fi
 
 export DEBIAN_FRONTEND=noninteractive
 
+INSTALL_PKGS="ntp git perl debhelper gpac"
+
 BUILD_TIME=`stat -c %Y /boot/kernel.img`
 case $BUILD_TIME in
   1523086824)
-    echo "Detect Crankshaft image 2018-04-07, disabling auto-shutdown..."
+    echo "Detect Crankshaft image 2018-04-07, disabling auto-shutdown and skip NTP..."
     shutdown -c
+    INSTALL_PKGS=${INSTALL_PKGS/ntp /}
     ;;
 esac
 
 echo "Installing packages..."
 apt-get update -y
-apt-get install -y ntp git perl
+apt-get install -y $INSTALL_PKGS
 
-echo "Setup NTP time sync..."
-systemctl enable ntp
-timedatectl set-ntp 1
+if [[ $INSTALL_PKGS == *"ntp "* ]]; then
+  echo "Setup NTP time sync..."
+  systemctl enable ntp
+  timedatectl set-ntp 1
+fi
+
+echo "Getting and Building USBmount..."
+cd $HOME
+git clone https://github.com/rbrito/usbmount.git
+cd usbmount
+rm -f ../usbmount_*.deb
+dpkg-buildpackage -us -uc -b
+apt install  ../usbmount_*.deb
 
 echo "Cloning Open Auto Pi Utilities repository..."
 cd $HOME
 git clone https://github.com/snailium/OpenAutoPiUtils.git
+
+echo "Setup not finished yet..."
+
+echo "Now use 'sudo reboot' to reboot system."
+
