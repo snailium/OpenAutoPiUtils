0680f3d86a9ed5a8b2aad879a0fd19edc9eea103
snailium <snailium@hotmail.com>
2018-05-21 07:56:02 -0400

    Tried to build up data base with timestamp of autoapp. Seems not that reliable.

--- setup.sh
+++ setup.sh
@@ -21,13 +21,22 @@ export DEBIAN_FRONTEND=noninteractive
 
 INSTALL_PKGS="ntp git i2c-tools perl bc debhelper gpac"
 
-BUILD_TIME=`stat -c %Y /boot/kernel.img`
+BUILD_TIME=`stat -c %Y /usr/local/bin/autoapp`
 # Crankshaft build time database
 #
 # Version    Date           Time
 # -------    ----------     ----------
-# v0.2.2     2018-04-07     1523086824
-# v0.2.3     2018-14-18     1524089656
+# v0.1.0     2018-02-24     1519516837
+# v0.1.1     2018-02-28     1519861911
+# v0.1.5     2018-03-02     1520047217
+# v0.1.6     2018-03-05     1520238100
+# v0.1.7     2018-03-10     1520658814
+# v0.2.0     2018-03-13     1520928566
+# v0.2.1-0   2018-04-01     1522564529
+# v0.2.1-1   2018 04-01     1522599276
+# v0.2.2     2018-04-07     1522552897
+# v0.2.3     2018-04-18     1522552897
+# v0.2.4     2018-05-18     1526619131
 case $BUILD_TIME in
   1523086824)
     echo "Detect Crankshaft image v0.2.2 (2018-04-07), disabling auto-shutdown and skip NTP..."
@@ -46,7 +55,7 @@ if [[ $INSTALL_PKGS == *"ntp "* ]]; then
   timedatectl set-ntp 1
 fi
 
-echo "Fetching, Building and Installing USBmount..."
+echo "Fetching, building and installing USBmount..."
 cd $HOME
 git clone https://github.com/rbrito/usbmount.git
 cd usbmount
@@ -58,12 +67,14 @@ echo "Cloning Open Auto Pi Utilities repository..."
 cd $HOME
 sudo -H -u pi git clone ${UTIL_REPO} ${UTIL_ROOT}
 
-echo "Enabling Open Auto Pi Utilities repository..."
-if [ ${BUILD_TIME} -ge 1524089656 ]; then
-  # For Crankshaft v0.2.3 and later, install scripts into /boot/crankshaft/startup.sh
-  STARTUP_SCRIPT=/boot/crankshaft/startup.sh
+echo "Enabling Open Auto Pi Utilities..."
+# For Crankshaft v0.2.3 and later, install scripts into /boot/crankshaft/startup.sh
+CRANKSHAFT_STARTUP=/boot/crankshaft/startup.sh
+if [ -e ${CRANKSHAFT_STARTUP} ]; then
   /opt/crankshaft/filesystem.sh unlock_boot
-  echo "${UTIL_ROOT}/brightness.sh &" | tee -a ${STARTUP_SCRIPT}
+  echo "${UTIL_ROOT}/brightness.sh &" | tee -a ${CRANKSHAFT_STARTUP}
+else
+  #TODO install to /etc/rc.local
 fi
 
 echo "Enabling Raspberry Pi I2C interface..."
